341bd8c811e8b04c7c08b476583a94d37cdc75d5
loserstar <490195824@qq.com>
2015-08-03 14:22:04 +0800

    完成购买课程页面

--- scripts/controllers/course_buy.js
+++ scripts/controllers/course_buy.js
@@ -1,51 +1,44 @@
 angular.module('sbAdminApp')
 .controller('courseBuyCtrl',
-function ($scope, $http, $state, $timeout, Restangular) {
-  $scope.login().then(function (){
-  $scope.school = $scope.currentUser.$related.school;
-  //$scope.majors = Restangular.all('schools/'+$scope.school.$id.toString()+'/majors').getList().$object;
-  var original;
-  return $scope.user = {
-      $type: "student",
-      $relationships: {
-        school: {data:$scope.school.$asLink()},
-      },
-      username: "",
-      password: "",
-      email: "",
-      code: "",
-      gender:"",
-      truename: "",
-      department: "",
-      title: "",
-      phone: "",
-      credit: "",
-      level: "",
-      address: "",
-      gendername:""
-  },
-  $scope.showInfoOnSubmit = !1,
-  original = angular.copy($scope.user),
-  $scope.revert = function() {
-      return $scope.user = angular.copy(original),
-      $scope.form_signin.$setPristine()
-  },
-  $scope.canRevert = function() {
-      return ! angular.equals($scope.user, original) || !$scope.form_signin.$pristine
-  },
-  $scope.canSubmit = function() {
-      return $scope.form_signin.$valid && !angular.equals($scope.user, original)
-  },
-  $scope.submitForm = function() {
-    console.log($scope.user);
-    if($scope.user.gendername == '男')
-      $scope.user.gender = 1;
-    else
-      $scope.user.gender = 2;
-    Restangular.all('students').post($scope.user).then(function (question) {
-          alert("新增学生成功");
-          return $scope.showInfoOnSubmit = !0,
-          $scope.revert();
+function($scope, $http, $state, $timeout, Restangular) {
+  $scope.login().then(function() {
+    $scope.school = $scope.currentUser.$related.school;
+    $scope.coursesAll = Restangular.all('courses?size=100000').getList().$object;
+    $scope.coursesExisted = Restangular.all('schools/' + $scope.school.$id.toString() + '/courses').getList().$object;
+    $scope.courseEnum = {
+      1: '公共基础课',
+      2: '专业核心课',
+      3: '实训课',
+      8: '模板课',
+    }
+
+    $scope.updateExisted = function() {
+      $scope.courseExisted = $scope.selectedcourseExisted.value[0];
+    };
+
+    $scope.updateAll = function() {
+      $scope.courseAll = $scope.selectedcourseAll.value[0];
+    };
+
+    $scope.buyCourse = function() {
+      var reqAdd = {
+        method: 'POST',
+        url: '/api/schools/' + $scope.school.$id.toString() + '/links/courses',
+        headers: {
+          'Access-Token': $scope.currentUser.$token,
+        },
+        data: {
+          "data": [{
+            type: "course",
+            id: $scope.courseAll.$id
+          }]
+        }
+      };
+      console.log(reqAdd);
+      $http(reqAdd)
+        .then(function() {
+          alert("添加指定课程成功");
+          $scope.coursesExisted = Restangular.all('schools/' + $scope.school.$id.toString() + '/courses').getList().$object;
         });
     };
   });
--- scripts/controllers/teacher_course.js
+++ scripts/controllers/teacher_course.js
@@ -1,75 +1,65 @@
 angular.module('sbAdminApp')
 .controller('teacherCourseCtrl',
 function ($scope, $http, $state, $timeout, $modalInstance, Restangular, school, teacher, currentUser) {
-    $scope.closeModal = function(){
+    $scope.closeModal = function() {
       $modalInstance.close();
     };
 
     $scope.school = school;
     $scope.teacher = teacher;
-    //console.log($scope.teacher);
-    $scope.coursesExisted = Restangular.all('teachers/'+$scope.teacher.$id.toString()+'/courses').getList().$object;
-    $scope.coursesAll = Restangular.all('majors/'+$scope.teacher.$relationships.major.data.id.toString()+'/courses').getList().$object;
-    //console.log($scope.majors);
-    //console.log($scope.major);
+    $scope.coursesExisted = Restangular.all('teachers/' + $scope.teacher.$id.toString() + '/courses').getList().$object;
+    $scope.coursesAll = Restangular.all('majors/' + $scope.teacher.$relationships.major.data.id.toString() + '/courses').getList().$object;
 
     $scope.updateExisted = function() {
       $scope.courseExisted = $scope.selectedcourseExisted.value[0];
-
     };
 
-    $scope.delCourse = function () {
+    $scope.delCourse = function() {
       console.log($scope.courseExisted);
       var reqDEL = {
-      method: 'DELETE',
-      url: '/api/teachers/'+$scope.teacher.$id.toString()+'/links/courses',
-      headers: {
-       'Access-Token': currentUser.$token,
-      },
-      data: {
-          "data":[
-            {
-              type:"course",
-              id: $scope.courseExisted.$id
-            }
-          ]
+        method: 'DELETE',
+        url: '/api/teachers/' + $scope.teacher.$id.toString() + '/links/courses',
+        headers: {
+          'Access-Token': currentUser.$token,
+        },
+        data: {
+          "data": [{
+            type: "course",
+            id: $scope.courseExisted.$id
+          }]
         }
       };
       console.log(reqDEL);
       $http(reqDEL)
-        .then(function () {
-            alert("删除指定课程成功");
-            $scope.coursesExisted = Restangular.all('teachers/'+$scope.teacher.$id.toString()+'/courses').getList().$object;
-          });
+        .then(function() {
+          alert("删除指定课程成功");
+          $scope.coursesExisted = Restangular.all('teachers/' + $scope.teacher.$id.toString() + '/courses').getList().$object;
+        });
     };
 
     $scope.updateAll = function() {
       $scope.courseAll = $scope.selectedcourseAll.value[0];
     };
 
-    $scope.addCourse = function () {
+    $scope.addCourse = function() {
       var reqAdd = {
-      method: 'POST',
-      url: '/api/teachers/'+$scope.teacher.$id.toString()+'/links/courses',
-      headers: {
-       'Access-Token': currentUser.$token,
-      },
-      data: {
-          "data":[
-            {
-              type:"course",
-              id: $scope.courseAll.$id
-            }
-          ]
+        method: 'POST',
+        url: '/api/teachers/' + $scope.teacher.$id.toString() + '/links/courses',
+        headers: {
+          'Access-Token': currentUser.$token,
+        },
+        data: {
+          "data": [{
+            type: "course",
+            id: $scope.courseAll.$id
+          }]
         }
       };
       console.log(reqAdd);
       $http(reqAdd)
-        .then(function () {
-            alert("添加指定课程成功");
-            $scope.coursesExisted = Restangular.all('teachers/'+$scope.teacher.$id.toString()+'/courses').getList().$object;
-          });
+        .then(function() {
+          alert("添加指定课程成功");
+          $scope.coursesExisted = Restangular.all('teachers/' + $scope.teacher.$id.toString() + '/courses').getList().$object;
+        });
     };
-
-
 });
